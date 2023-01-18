c35066fc88910b1d7c1d06fc5cd65e3f745a8339
Bartosz Laskowski <81393810+Nougat00@users.noreply.github.com>
2023-01-19 00:59:07 +0100

    added fllash message and navbar

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, url_for, redirect, abort
+from flask import Flask, render_template, url_for, redirect, abort, flash
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
-from sqlalchemy import  create_engine
 from flask_wtf import FlaskForm
 from flask_bcrypt import generate_password_hash, check_password_hash
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-import pyodbc
-import urllib
 
 
 app = Flask(__name__)
@@ -86,12 +83,12 @@ def load_user(user_id):
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
-        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Nazwa użytkownika"})
 
     password = PasswordField(validators=[
-        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Hasło"})
 
-    submit = SubmitField('Register')
+    submit = SubmitField('Zarejestruj się')
 
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(
@@ -103,12 +100,12 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[
-        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Nazwa użytkownika"})
 
     password = PasswordField(validators=[
-        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Hasło"})
 
-    submit = SubmitField('Login')
+    submit = SubmitField('Zaloguj się')
 
 
 @app.route('/')
@@ -125,6 +122,10 @@ def login():
             if check_password_hash(user.password, form.password.data):
                 login_user(user)
                 return redirect(url_for('dashboard'))
+            else:
+                flash("Nieprawidłowy login lub hasło", "warning")
+        else:
+            flash("Nieprawidłowy login lub hasło", "warning")
     return render_template('login.html', form=form)
 
 
@@ -145,14 +146,14 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('login'))
-
+    else:
+        flash("Wybrana nazwa użytkownika jest już zajęta", "warning")
     return render_template('register.html', form=form)
 
 
@@ -181,9 +182,6 @@ schools = (
 schools_by_key = {school.key: school for school in schools}
 
 
-@app.route("/shools")
-def main():
-    return render_template('main.html', schools=schools)
 
 
 @app.route("/<school_code>")
